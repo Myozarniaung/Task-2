69b6375f9e6e494cf394293e832d13c1eac2bf74
Myozarniaung <myozarniaung1990@gmail.com>
2022-02-28 21:42:34 +0800

    Add files via upload

---
+++ orderconsumerapi/Models/orderconsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace orderconsumerapi.Models
+{
+    public class orderconsumer
+    {
+        public int cartId { get; set; }
+        public int productId { get; set; }
+        public double price { get; set; }
+        public int total { get; set; }
+        public string orderId { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ orderconsumerapi/Models/orderconsumerDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace orderconsumerapi.Models
+{
+    public class orderconsumerDBContext: DbContext
+    {
+        public orderconsumerDBContext(DbContextOptions<orderconsumerDBContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<orderconsumer> orderconsumer { get; set; }
+    }
+}
+
