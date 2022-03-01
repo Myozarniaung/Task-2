77301c7674b9d520aa85ee9ab9af5b242d2efe78
CPPS WP3.4 <cp3-4@gmail.com>
2022-03-01 23:43:17 +0800

    commit

--- orderconsumerapi/Models/orderconsumer.cs
+++ orderconsumerapi/Models/orderconsumer.cs
@@ -8,10 +8,14 @@ namespace orderconsumerapi.Models
     public class orderconsumer
     {
         public int cartId { get; set; }
-        public int productId { get; set; }
-        public double price { get; set; }
         public int total { get; set; }
         public string orderId { get; set; }
         public string Status { get; set; }
+        public List<product> products { get; set; }
+    }
+    public class product
+    {
+        public string productId { get; set; }
+        public double price { get; set; }
     }
 }
